0855e0f6bb5aad05f88f126452cba8a0f66aed79
Jeon Ye Won <yewon1118@naver.com>
2023-09-08 11:37:07 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.py
@@ -0,0 +1,12 @@
+def solution(d, budget):
+    answer = 0
+    d.sort()
+    
+    for i in d:
+        budget -= i
+        if budget >= 0:
+            answer += 1
+        else:
+            break
+    
+    return answer
